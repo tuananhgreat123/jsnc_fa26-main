4dbf7d767e8bb239ef18ae836eb994d0a70ab12f
tuancode01 <tuananhgreat123@gmail.com>
2026-09-15 15:54:33 +0700

    Xong lap 1 phan1

--- lab1.js
+++ lab1.js
@@ -1,1 +1,13 @@
 console.log("Day la lab1.js");
+// Khai báo biến
+
+let name = "Tuấn Anh";
+let age = 19;
+let address = "Thanh Hóa";
+let isStudent = true;
+
+// các biến ra Console
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
