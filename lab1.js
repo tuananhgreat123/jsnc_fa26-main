32fc3de4ed8716181293362bfd1bd6095826125f
tuancode01 <tuananhgreat123@gmail.com>
2026-09-17 15:31:10 +0700

    Hoàn thành bài tập

--- lab1.js
+++ lab1.js
@@ -11,3 +11,23 @@ console.log("Họ tên:", name);
 console.log("Tuổi:", age);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
+// Bai 2 : Thay đổi giá trị của biến
+let age2 = 20;
+console.log(age2);
+age2 = 21; 
+console.log(age2);
+// bai 3: Tinh toan voi bien
+const a = 10;
+const b = 5;
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+// bai 4 : Hàm chào hỏi
+function sayHello(name){
+    console.log("Xin Chào " + name);
+}
+sayHello("Tuấn Anh");
+sayHello("Bình");
+sayHello("Hải");
+sayHello("Hà");
