6c400cd16a9628d7fa92718c3b062b3789ab3dea
tuancode01 <tuananhgreat123@gmail.com>
2026-09-24 15:31:35 +0700

    Ht3

--- lab3.js
+++ lab3.js
@@ -16,7 +16,16 @@ const myName = `- Ten Toi la: ${name}
                         -Ten: ${student.name}
                         -Tuoi: ${student.age}`;
     console.log(studentInfo);
-    // Bai Tap
+    //25 - Template Litera
+    const name1 = "tuancode";
+    const age1 = 20;
+    const major1 = "IT";
+    const info1 = ` - Ten: ${name1}
+                    - Tuoi: ${age1}
+                    - Nganh: ${major1}`;
+    console.log(info1);
+
+    // Bai Tap 26
     const student1 = {
         name: "tuancode",
         age:20,
@@ -26,11 +35,34 @@ const myName = `- Ten Toi la: ${name}
                         - Tuoi: ${student1.age}
                         - Nganh: ${student1.major}`;
         console.log(studentInfo1);
-//
+// 27
 function add(a,b){
     return a + b;
 }
-//
+
 const result = add(10,20); 
-const resultInfo = `Ket qua: ${result}`;
+const resultInfo = `Ket qua cua a va b: ${result}`;
 console.log(resultInfo);
+// 28 Arrow Function  +Viết các Arrow Function:
+// a. Tinh binh phong
+const square = (x) => x * x;
+console.log(square(5));
+// b. tinh tong
+const sum = (a,b) => a + b;
+console.log(sum(10,20));
+// c. Chao nguoi dungs
+function sayHello(name){
+console.log(name)
+}
+sayHello("Xin chao tuan");
+//  29
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((n)=>n*2);
+console.log(doubled);
+// bai 30
+const students = [
+    { id: 1, name: "An"},
+    { id: 2, name: "Tuan"},
+    { id: 3, name: "PhAn"},
+];
+
